26e5a5abd47aca1f9548e41b374cd0d32bdcff57
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2025-12-19 09:40:59 +0700

    push

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import Pricing from "../(home)/pricing";
+
+export default function PricingPage() {
+	return (
+		<div className="container mx-auto py-8">
+			<Pricing />
+		</div>
+	);
+}
---
+++ src/app/resources/page.tsx
@@ -0,0 +1,8 @@
+export default function ResourcesPage() {
+	return (
+		<div className="container mx-auto py-8">
+			<h1>Resources</h1>
+			<p>Resources content will be added here.</p>
+		</div>
+	);
+}
