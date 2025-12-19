3049b96e60d816797a2a5b51eef4142806dac096
wahyu ikbal maulana <wahyuikbal777@gmail.com>
2025-12-19 09:45:43 +0700

    deploy

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -11,13 +11,10 @@ declare namespace Cloudflare {
 }
 interface CloudflareEnv extends Cloudflare.Env {}
 type StringifyValues<EnvType extends Record<string, unknown>> = {
-	[Binding in keyof EnvType]: EnvType[Binding] extends string
-		? EnvType[Binding]
-		: string;
+	[Binding in keyof EnvType]: EnvType[Binding] extends string ? EnvType[Binding] : string;
 };
 declare namespace NodeJS {
-	interface ProcessEnv
-		extends StringifyValues<Pick<Cloudflare.Env, "NEXTJS_ENV">> {}
+	interface ProcessEnv extends StringifyValues<Pick<Cloudflare.Env, "NEXTJS_ENV">> {}
 }
 
 // Begin runtime types
